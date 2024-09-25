3b1ebee361976ef58be1dcba9ee42e76ad3bcf01
osman yaycioglu <osman.yaycioglu@gmail.com>
2024-09-25 14:45:04 +0300

    day 3

---
+++ ms-common/src/main/java/org/turkcell/training/spring/mscommon/MsCommonApplication.java
@@ -0,0 +1,14 @@
+package org.turkcell.training.spring.mscommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCommonApplication.class,
+                              args);
+    }
+
+}
