7bd2342d840224bdeb8426b3366009adb5eb3f9f
osman yaycioglu <osman.yaycioglu@gmail.com>
2024-09-26 15:02:11 +0300

    day 5

---
+++ ms-restaurant/src/test/java/org/turkcell/training/spring/restaurant/MsRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package org.turkcell.training.spring.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
