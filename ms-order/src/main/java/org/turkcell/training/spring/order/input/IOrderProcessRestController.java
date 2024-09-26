93f845838e09ad66d8ec4998cbd788e6146d58f3
osman yaycioglu <osman.yaycioglu@gmail.com>
2024-09-26 09:27:52 +0300

    day 4

---
+++ ms-order/src/main/java/org/turkcell/training/spring/order/input/IOrderProcessRestController.java
@@ -0,0 +1,19 @@
+package org.turkcell.training.spring.order.input;
+
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.turkcell.training.spring.order.input.models.OrderDto;
+import org.turkcell.training.spring.order.input.models.OrderResponse;
+
+@RequestMapping("/api/v1/order/process")
+public interface IOrderProcessRestController {
+
+    @PostMapping("/place")
+    @Operation(summary = "Order yaratmak için",
+            description = "uzun açılamalar falan filan")
+    OrderResponse placeOrder(@Valid @RequestBody OrderDto orderDtoParam);
+
+}
--- ms-order/src/main/java/org/turkcell/training/spring/order/input/OrderProcessRestController.java
+++ ms-order/src/main/java/org/turkcell/training/spring/order/input/OrderProcessRestController.java
@@ -1,18 +1,14 @@
 package org.turkcell.training.spring.order.input;
 
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.turkcell.training.spring.order.input.models.OrderDto;
 import org.turkcell.training.spring.order.input.models.OrderResponse;
 
 @RestController
-@RequestMapping("/api/v1/order/process")
-public class OrderProcessRestController {
+public class OrderProcessRestController implements IOrderProcessRestController {
 
-    @PostMapping("/place")
     public OrderResponse placeOrder(@Valid @RequestBody OrderDto orderDtoParam) {
         return null;
     }
