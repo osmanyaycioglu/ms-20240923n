d28d7e24e8cdd91be67325b71fb91ae1e7841ece
osman yaycioglu <osman.yaycioglu@gmail.com>
2024-09-27 15:47:10 +0300

    day 5

---
+++ ms-notify/src/main/java/org/turkcell/training/spring/msnotify/MsNotifyApplication.java
@@ -0,0 +1,14 @@
+package org.turkcell.training.spring.msnotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNotifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsNotifyApplication.class,
+                              args);
+    }
+
+}
---
+++ ms-notify/src/test/java/org/turkcell/training/spring/msnotify/MsNotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.turkcell.training.spring.msnotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNotifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
